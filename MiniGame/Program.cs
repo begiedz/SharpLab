36794eee01e6673750daf1d27b536a2018e4ef44
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2026-04-25 15:07:23 +0200

    chore: format document

--- MiniGame/Program.cs
+++ MiniGame/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-Random random = new Random();
+﻿Random random = new Random();
 Console.CursorVisible = false;
 int height = Console.WindowHeight - 1;
 int width = Console.WindowWidth - 5;
@@ -15,8 +13,8 @@ int foodX = 0;
 int foodY = 0;
 
 // Available player and food strings
-string[] states = {"('-')", "(^-^)", "(X_X)"};
-string[] foods = {"@@@@@", "$$$$$", "#####"};
+string[] states = { "('-')", "(^-^)", "(X_X)" };
+string[] foods = { "@@@@@", "$$$$$", "#####" };
 
 // Current player string displayed in the Console
 string player = states[0];
@@ -26,8 +24,9 @@ int food = 0;
 
 // === Game ===
 InitializeGame();
-
-while (!shouldExit) 
+int x = 0;
+if (x == 1) { Console.WriteLine("test"); }
+while (!shouldExit)
 {
     if (TerminalResized())
         endGame("Console was resized.");
@@ -41,13 +40,13 @@ while (!shouldExit)
 // === Game ===
 
 // Returns true if the Terminal was resized 
-bool TerminalResized() 
+bool TerminalResized()
 {
     return height != Console.WindowHeight - 1 || width != Console.WindowWidth - 5;
 }
 
 // Displays random food at a random location
-void ShowFood() 
+void ShowFood()
 {
     // Update food to a random index
     food = random.Next(0, foods.Length);
@@ -62,7 +61,7 @@ void ShowFood()
 }
 
 // Changes the player to match the food consumed
-void ChangePlayer() 
+void ChangePlayer()
 {
     player = states[food];
     Console.SetCursorPosition(playerX, playerY);
@@ -70,7 +69,7 @@ void ChangePlayer()
 }
 
 // Temporarily stops the player from moving
-void FreezePlayer() 
+void FreezePlayer()
 {
     System.Threading.Thread.Sleep(1000);
     player = states[0];
@@ -81,7 +80,7 @@ bool ConsumedFood()
     bool overlappedX = playerX == foodX;
     bool overlappedY = playerY == foodY;
 
-    if ( overlappedX && overlappedY )
+    if (overlappedX && overlappedY)
     {
         ChangePlayer();
         ShowFood();
@@ -105,42 +104,42 @@ bool isPlayerGoodState()
 {
     if (player == states[1])
         return true;
- 
+
     return false;
 }
 
 // Reads directional input from the Console and moves the player
-void Move(bool? terminateOnNonDirectionalInput = false, bool? increasedMovementSpeed = false) 
+void Move(bool? terminateOnNonDirectionalInput = false, bool? increasedMovementSpeed = false)
 {
     int lastX = playerX;
     int lastY = playerY;
 
-    switch (Console.ReadKey(true).Key) 
+    switch (Console.ReadKey(true).Key)
     {
         case ConsoleKey.UpArrow:
-            playerY--; 
+            playerY--;
             break;
-		case ConsoleKey.DownArrow: 
-            playerY++; 
+        case ConsoleKey.DownArrow:
+            playerY++;
             break;
-		case ConsoleKey.LeftArrow:  
+        case ConsoleKey.LeftArrow:
             if (increasedMovementSpeed == true)
             {
                 playerX -= 3;
                 break;
             }
-            playerX--; 
+            playerX--;
             break;
-		case ConsoleKey.RightArrow:
+        case ConsoleKey.RightArrow:
             if (increasedMovementSpeed == true)
             {
                 playerX += 3;
                 break;
             }
-            playerX++; 
+            playerX++;
             break;
-		case ConsoleKey.Escape:     
-            shouldExit = true; 
+        case ConsoleKey.Escape:
+            shouldExit = true;
             break;
         default:
             if (terminateOnNonDirectionalInput == true)
@@ -150,7 +149,7 @@ void Move(bool? terminateOnNonDirectionalInput = false, bool? increasedMovementS
 
     // Clear the characters at the previous position
     Console.SetCursorPosition(lastX, lastY);
-    for (int i = 0; i < player.Length; i++) 
+    for (int i = 0; i < player.Length; i++)
     {
         Console.Write(" ");
     }
@@ -165,7 +164,7 @@ void Move(bool? terminateOnNonDirectionalInput = false, bool? increasedMovementS
 }
 
 // Clears the console, displays the food and player
-void InitializeGame() 
+void InitializeGame()
 {
     Console.Clear();
     ShowFood();
