e305788b122de7811350ef16c55b93484d29a54c
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2026-04-28 09:40:47 +0200

    chore: exception types

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -15,19 +15,37 @@ static void Process1()
     {
         WriteMessage();
     }
-    catch
+    catch (DivideByZeroException ex)
     {
-        Console.WriteLine("Exception caught in Process1");
+        Console.WriteLine($"Exception caught in Process1: {ex.Message}");
     }
 }
-
 static void WriteMessage()
 {
     double float1 = 3000.0;
     double float2 = 0.0;
     int number1 = 3000;
     int number2 = 0;
+    byte smallNumber;
 
-    Console.WriteLine(float1 / float2);
-    Console.WriteLine(number1 / number2);
+    try
+    {
+        Console.WriteLine(float1 / float2);
+        Console.WriteLine(number1 / number2);
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine($"Exception caught in WriteMessage: {ex.Message}");
+    }
+    checked
+    {
+        try
+        {
+            smallNumber = (byte)number1;
+        }
+        catch (OverflowException ex)
+        {
+            Console.WriteLine($"Exception caught in WriteMessage: {ex.Message}");
+        }
+    }
 }
