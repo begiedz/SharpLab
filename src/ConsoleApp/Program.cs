bcf2d31d8141add4ab98c034d42a835c6517ecb6
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2026-04-28 10:28:07 +0200

    chore: exception challange

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,51 +1,48 @@
 ﻿try
 {
-    Process1();
+    int num1 = int.MaxValue;
+    int num2 = int.MaxValue;
+    int result = checked(num1 + num2);
+    Console.WriteLine("Result: " + result);
 }
-catch
+catch (OverflowException ex)
 {
-    Console.WriteLine("An exception has occurred");
+    Console.WriteLine("Error: The number is too large to be represented as an integer." + ex.Message);
 }
 
-Console.WriteLine("Exit program");
 
-static void Process1()
+try
 {
-    try
-    {
-        WriteMessage();
-    }
-    catch (DivideByZeroException ex)
-    {
-        Console.WriteLine($"Exception caught in Process1: {ex.Message}");
-    }
+    string str = null;
+    int length = str.Length;
+    Console.WriteLine("String Length: " + length);
 }
-static void WriteMessage()
+catch (NullReferenceException ex)
 {
-    double float1 = 3000.0;
-    double float2 = 0.0;
-    int number1 = 3000;
-    int number2 = 0;
-    byte smallNumber;
+    Console.WriteLine("Error: The reference is null." + ex.Message);
+}
 
-    try
-    {
-        Console.WriteLine(float1 / float2);
-        Console.WriteLine(number1 / number2);
-    }
-    catch (DivideByZeroException ex)
-    {
-        Console.WriteLine($"Exception caught in WriteMessage: {ex.Message}");
-    }
-    checked
-    {
-        try
-        {
-            smallNumber = (byte)number1;
-        }
-        catch (OverflowException ex)
-        {
-            Console.WriteLine($"Exception caught in WriteMessage: {ex.Message}");
-        }
-    }
+try
+{
+    int[] numbers = new int[5];
+    numbers[5] = 10;
+    Console.WriteLine("Number at index 5: " + numbers[5]);
 }
+catch (IndexOutOfRangeException ex)
+{
+    Console.WriteLine("Error: Index out of range." + ex.Message);
+}
+try
+{
+
+    int num3 = 10;
+    int num4 = 0;
+    int result2 = num3 / num4;
+    Console.WriteLine("Result: " + result2);
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("Error: Cannot divide by zero." + ex.Message);
+}
+
+Console.WriteLine("Exiting program.");
